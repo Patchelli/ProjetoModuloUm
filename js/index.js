5e6b9cfe8544a154b0d9130eb389ec1685798338
Patrick Patchelli <patrick.patchelli@hotmail.com>
2022-04-24 23:30:54 -0300

    create perfil

---
+++ js/index.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    confirm("Após longo seis meses, você cumpriu sua missão nesse projeto. Agora é a hora de mostrar que atingiu a superioridade na programação.")
+    confirm("Escolha um pesrsonagem")
+}, 200);
+var today = new Date();
+var date = today.getFullYear();
+document.getElementById("currentDate").innerHTML = date;
